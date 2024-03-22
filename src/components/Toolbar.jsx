d9d3ffb22c73d73855a8e4fe43e3eb65cf2d6d8b
brenettg <brenettg@student.21-school.ru>
2024-03-22 19:37:03 +0300

    init: верстка, заготовка

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/toolbar.scss'
+
+const Toolbar = () => {
+    return (
+        <div className="toolbar">
+            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn eraser"></button>
+            <button className="toolbar__btn line"></button>
+            <button className="toolbar__btn rect"></button>
+            <input type="color"/>
+            <button className="toolbar__btn undo"></button>
+            <button className="toolbar__btn save"></button>
+            <button className="toolbar__btn redo"></button>
+
+        </div>
+    );
+};
+
+export default Toolbar;
