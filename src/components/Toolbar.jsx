8e862e0f09fa8385ac1c50846b35f67165719490
brenettg <brenettg@student.21-school.ru>
2024-03-22 22:38:11 +0300

    init: добавил квадрат

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/toolbar.scss'
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import Rect from "../tools/Rect";
 import canvas from "./Canvas";
 import canvasState from "../store/canvasState";
 
@@ -9,10 +10,10 @@ const Toolbar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}></button>
-            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn rect " onClick={()=> toolState.setTool(new Rect(canvasState.canvas))}></button>
             <button className="toolbar__btn eraser"></button>
             <button className="toolbar__btn line"></button>
-            <button className="toolbar__btn rect"></button>
+            <button className="toolbar__btn circle"></button>
             <input type="color" style={{marginLeft:"10px"}} />
             <button className="toolbar__btn undo"></button>
             <button className="toolbar__btn save"></button>
