f8c460ce786e179a1fbf015d4425799748c7020e
brenettg <brenettg@student.21-school.ru>
2024-03-22 21:36:38 +0300

    init: mobx + рисовалка(кисть)

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../styles/toolbar.scss'
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
+import canvas from "./Canvas";
+import canvasState from "../store/canvasState";
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
-            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}></button>
             <button className="toolbar__btn circle"></button>
             <button className="toolbar__btn eraser"></button>
             <button className="toolbar__btn line"></button>
             <button className="toolbar__btn rect"></button>
-            <input type="color"/>
+            <input type="color" style={{marginLeft:"10px"}} />
             <button className="toolbar__btn undo"></button>
             <button className="toolbar__btn save"></button>
             <button className="toolbar__btn redo"></button>
